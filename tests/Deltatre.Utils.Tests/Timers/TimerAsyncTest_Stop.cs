470cc99779e094c15d25adc759459553702d8744
Enrico Massone <enrico.massone@deltatre.it>
2018-10-23 10:52:04 +0200

    work in progress: testing stop method for timer async

--- tests/Deltatre.Utils.Tests/Timers/TimerAsyncTest_Stop.cs
+++ tests/Deltatre.Utils.Tests/Timers/TimerAsyncTest_Stop.cs
@@ -1,4 +1,5 @@
-﻿using Deltatre.Utils.Timers;
+﻿using Deltatre.Utils.Extensions.Enumerable;
+using Deltatre.Utils.Timers;
 using NUnit.Framework;
 using System;
 using System.Collections.Concurrent;
@@ -102,7 +103,62 @@ namespace Deltatre.Utils.Tests.Timers
       CollectionAssert.AreEqual(snapshot1, snapshot2);
     }
 
+    [Test]
+    public async Task Stop_Is_Thread_Safe()
+    {
+      // ARRANGE
+      var values = new ConcurrentBag<int>();
+      Func<CancellationToken, Task> action = ct =>
+      {
+        ct.ThrowIfCancellationRequested();
+        values.Add(1);
+        return Task.CompletedTask;
+      };
 
+      var target = new TimerAsync(
+        action,
+        TimeSpan.FromMilliseconds(500),
+        TimeSpan.FromMilliseconds(500));
+
+      bool run = true;
+
+      var thread1 = new Thread(() =>
+      {
+        while (run)
+        {
+          target.Stop().Wait();
+        }
+      });
+
+      var thread2 = new Thread(() =>
+      {
+        while (run)
+        {
+          target.Stop().Wait();
+        }
+      });
+
+      var threads = new[] { thread1, thread2 };
+
+      // ACT
+      target.Start();
+
+      await Task.Delay(1200).ConfigureAwait(false); // in 1200 milliseconds we are sure that action is called at least twice
+
+      threads.ForEach(t => t.Start());
+      await Task.Delay(2000).ConfigureAwait(false);
+      run = false;
+      threads.ForEach(t => t.Join());
+      var snapshot1 = values.ToArray();
+
+      await Task.Delay(1200).ConfigureAwait(false); // in 1200 milliseconds we are sure that action is called at least twice (but now the timer is stopped)
+      var snapshot2 = values.ToArray();
+
+      // ASSERT
+      Assert.GreaterOrEqual(snapshot1.Length, 2);
+      Assert.IsTrue(snapshot1.All(i => i == 1));
+      CollectionAssert.AreEqual(snapshot1, snapshot2);
+    }
 
 
 
