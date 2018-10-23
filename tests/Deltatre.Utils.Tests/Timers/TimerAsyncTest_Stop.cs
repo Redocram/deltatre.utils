536a0a383d9e9db33e7a4a26a15e9c5941d5bb77
Enrico Massone <enrico.massone@deltatre.it>
2018-10-23 10:08:43 +0200

    work in progress

--- tests/Deltatre.Utils.Tests/Timers/TimerAsyncTest_Stop.cs
+++ tests/Deltatre.Utils.Tests/Timers/TimerAsyncTest_Stop.cs
@@ -46,6 +46,77 @@ namespace Deltatre.Utils.Tests.Timers
     }
 
     [Test]
+    public void Stop_Can_Be_Called_More_Than_Once_Without_Throwing_Exceptions()
+    {
+      // ARRANGE
+      var target = new TimerAsync(
+        _ => Task.CompletedTask,
+        TimeSpan.FromMilliseconds(500),
+        TimeSpan.FromMilliseconds(500));
+
+      // ASSERT
+      Assert.DoesNotThrowAsync(async () =>
+      {
+        target.Start();
+
+        await Task.Delay(1200).ConfigureAwait(false);
+
+        await target.Stop().ConfigureAwait(false);
+        await target.Stop().ConfigureAwait(false);
+      });
+    }
+
+    [Test]
+    public async Task Stop_Can_Be_Called_More_Than_Once_Without_Changing_Expected_Behaviour()
+    {
+      // ARRANGE
+      var values = new ConcurrentBag<int>();
+      Func<CancellationToken, Task> action = ct =>
+      {
+        ct.ThrowIfCancellationRequested();
+        values.Add(1);
+        return Task.CompletedTask;
+      };
+
+      var target = new TimerAsync(
+        action,
+        TimeSpan.FromMilliseconds(500),
+        TimeSpan.FromMilliseconds(500));
+
+      // ACT
+      target.Start();
+
+      await Task.Delay(1200).ConfigureAwait(false); // in 1200 milliseconds we are sure that action is called at least twice
+
+      await target.Stop().ConfigureAwait(false);
+      await target.Stop().ConfigureAwait(false);
+      var snapshot1 = values.ToArray();
+
+      await Task.Delay(1200).ConfigureAwait(false); // in 1200 milliseconds we are sure that action is called at least twice (but now the timer is stopped)
+      await target.Stop().ConfigureAwait(false);
+      var snapshot2 = values.ToArray();
+
+      // ASSERT
+      Assert.GreaterOrEqual(snapshot1.Length, 2);
+      Assert.IsTrue(snapshot1.All(i => i == 1));
+      CollectionAssert.AreEqual(snapshot1, snapshot2);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    [Test]
     public void Calling_Stop_Before_Start_Should_Not_Throw_Exception()
     {
       // ARRANGE
@@ -55,17 +126,7 @@ namespace Deltatre.Utils.Tests.Timers
       Assert.DoesNotThrowAsync(async () => await timer.Stop());
     }
 
-    [Test]
-    public async Task Calling_Stop_More_Than_Once_Should_Not_Throw_Exception()
-    {
-      // ARRANGE
-      var timer = new TimerAsync(_ => Task.FromResult(true), TimeSpan.FromMilliseconds(500), TimeSpan.FromMilliseconds(500));
-      timer.Start();
-
-      // ASSERT
-      await timer.Stop();
-      Assert.DoesNotThrowAsync(async () => await timer.Stop());
-    }
+    
 
     [Test]
     public async Task Calling_Stop_Before_Start_Should_Not_Change_Running_Behaviour()
