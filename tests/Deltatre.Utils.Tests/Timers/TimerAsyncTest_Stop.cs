e89c61944f988efb58b40cbdb6ef04ec39398aea
Enrico Massone <enrico.massone@deltatre.it>
2018-10-23 12:32:13 +0200

    completed the test suite for stop method of timer async

--- tests/Deltatre.Utils.Tests/Timers/TimerAsyncTest_Stop.cs
+++ tests/Deltatre.Utils.Tests/Timers/TimerAsyncTest_Stop.cs
@@ -3,7 +3,6 @@ using Deltatre.Utils.Timers;
 using NUnit.Framework;
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -174,53 +173,57 @@ namespace Deltatre.Utils.Tests.Timers
       Assert.ThrowsAsync<ObjectDisposedException>(timer.Stop);
     }
 
-
-
-
-
-
-
-
-
-
-
     [Test]
-    public void Calling_Stop_Before_Start_Should_Not_Throw_Exception()
+    public void Stop_Can_Be_Called_Before_Start_Without_Throwing_Exceptions()
     {
       // ARRANGE
-      var timer = new TimerAsync(_ => Task.FromResult(true), TimeSpan.FromMilliseconds(500), TimeSpan.FromMilliseconds(500));
+      var target = new TimerAsync(
+        _ => Task.CompletedTask,
+        TimeSpan.FromMilliseconds(500),
+        TimeSpan.FromMilliseconds(500));
 
       // ASSERT
-      Assert.DoesNotThrowAsync(async () => await timer.Stop());
+      Assert.DoesNotThrowAsync(async () =>
+      {
+        await target.Stop().ConfigureAwait(false);
+        target.Start();
+        await Task.Delay(1200).ConfigureAwait(false);
+      });
     }
 
-    
-
     [Test]
-    public async Task Calling_Stop_Before_Start_Should_Not_Change_Running_Behaviour()
+    public async Task Stop_Can_Be_Called_Before_Start_Without_Changing_Expected_Behaviour()
     {
       // ARRANGE
-      var list = new List<int>();
+      var values = new ConcurrentBag<int>();
       Func<CancellationToken, Task> action = ct =>
       {
         ct.ThrowIfCancellationRequested();
-        list.Add(1);
-        return Task.FromResult(true);
+        values.Add(1);
+        return Task.CompletedTask;
       };
-      var timer = new TimerAsync(action, TimeSpan.FromMilliseconds(500), TimeSpan.FromMilliseconds(500));
-      await timer.Stop();
+
+      var target = new TimerAsync(
+        action,
+        TimeSpan.FromMilliseconds(500),
+        TimeSpan.FromMilliseconds(500));
 
       // ACT
-      timer.Start();
+      await target.Stop().ConfigureAwait(false);
+      target.Start();
+
+      await Task.Delay(1200).ConfigureAwait(false); // in 1200 milliseconds we are sure that action is called at least twice
+
+      await target.Stop().ConfigureAwait(false);
+      var snapshot1 = values.ToArray();
+
+      await Task.Delay(1200).ConfigureAwait(false); // in 1200 milliseconds we are sure that action is called at least twice (but now the timer is stopped)
+      var snapshot2 = values.ToArray();
 
       // ASSERT
-      await Task.Delay(1200);
-      Assert.GreaterOrEqual(2, list.Count);
-      Assert.IsTrue(list.All(i => i == 1));
+      Assert.GreaterOrEqual(snapshot1.Length, 2);
+      Assert.IsTrue(snapshot1.All(i => i == 1));
+      CollectionAssert.AreEqual(snapshot1, snapshot2);
     }
-
-    
-
-    
   }
 }
